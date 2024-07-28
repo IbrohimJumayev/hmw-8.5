02f98592b1c8b4befe2f932bedb538a7bd9a8ad0
Ibrohim Jumayev <ibrohimjumayev@MacBook-Air-Ibrohim.local>
2024-07-28 13:08:36 +0500

    v

--- app/home/page.js
+++ app/home/page.js
@@ -6,33 +6,7 @@ const page = () => {
       <h1 className="text-5xl font-bold text-indigo-400 text-center mt-10">
         Home Page
       </h1>
-      <p className="font-bold text-indigo-500 mt-10 text-center">
-        Welcome to our website! We're delighted to have you here. Discover
-        Innovation and Excellence Explore our world of cutting-edge solutions,
-        where passion meets purpose. Our dynamic team of visionaries, creators,
-        and doers is committed to making a positive impact through innovative
-        technology and unwavering quality. Our Journey From a humble startup to
-        a thriving organization, our journey is fueled by a relentless pursuit
-        of excellence and a deep belief in the power of innovation. We're
-        dedicated to solving real-world problems and creating meaningful
-        experiences for our clients. Meet Our Team Our success is driven by our
-        diverse team of professionals, each bringing unique perspectives and
-        expertise. Our collaborative culture fosters creativity and encourages
-        out-of-the-box thinking, enabling us to achieve the extraordinary
-        together. Commitment to Quality Quality is at the heart of everything we
-        do. We meticulously craft every detail, ensuring our solutions meet the
-        highest standards of excellence. Our dedication to quality extends
-        beyond our products and services, reflecting in our company culture and
-        business conduct. Sustainability and Social Responsibility We're
-        committed to making a positive impact on society and the environment.
-        From eco-friendly practices to supporting community initiatives, we take
-        our responsibility seriously and strive to create a better world. Join
-        Us on Our Journey Thank you for visiting our website. We invite you to
-        explore, learn more about our innovative solutions, and join us in our
-        mission to create a brighter future. Whether you're a potential client,
-        partner, or simply curious about what we do, we're excited to share our
-        story with you. Welcome to our world of innovation and excellence!
-      </p>
+      <p className="font-bold text-indigo-500 mt-10 text-center">Home Page</p>
     </div>
   );
 };
